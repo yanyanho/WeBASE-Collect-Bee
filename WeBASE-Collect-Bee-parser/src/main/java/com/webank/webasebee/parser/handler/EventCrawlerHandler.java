b51c737b5a57f0937b2293952d590d80e582bf75
shuoyanhe <yanyanho@126.com>
2019-08-20 14:51:59 +0800

    add eventcontractAddress

--- WeBASE-Collect-Bee-parser/src/main/java/com/webank/webasebee/parser/handler/EventCrawlerHandler.java
+++ WeBASE-Collect-Bee-parser/src/main/java/com/webank/webasebee/parser/handler/EventCrawlerHandler.java
@@ -21,10 +21,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import com.webank.webasebee.common.constants.ContractConstants;
 import org.apache.commons.lang3.StringUtils;
 import org.fisco.bcos.web3j.protocol.core.methods.response.BcosBlock.Block;
 import org.fisco.bcos.web3j.protocol.core.methods.response.BcosBlock.TransactionResult;
 import org.fisco.bcos.web3j.protocol.core.methods.response.BcosTransactionReceipt;
+import org.fisco.bcos.web3j.protocol.core.methods.response.Transaction;
 import org.fisco.bcos.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -61,6 +63,15 @@ public class EventCrawlerHandler {
             if (opt.isPresent()) {
                 TransactionReceipt tr = opt.get();
                 String contractName = txHashContractNameMapping.get(tr.getTransactionHash());
+
+                Optional<Transaction> optt = ethClient.getTransactionByHash(tr);
+                if (optt.isPresent()) {
+                    Transaction transaction = optt.get();
+                    if (transaction.getTo() != null && !transaction.getTo().equals(ContractConstants.EMPTY_ADDRESS)) {
+                        tr.setContractAddress(transaction.getTo());
+                    }
+                }
+
                 if (StringUtils.isEmpty(contractName)) {
                     log.error("TxHash {} is Empty, and the blockNumber is {}! Please check it. ",
                             tr.getTransactionHash(), block.getNumber());
--- WeBASE-Collect-Bee-parser/src/main/java/com/webank/webasebee/parser/handler/MethodCrawlerHandler.java
+++ WeBASE-Collect-Bee-parser/src/main/java/com/webank/webasebee/parser/handler/MethodCrawlerHandler.java
@@ -107,7 +107,7 @@ public class MethodCrawlerHandler {
                             .getMethodCrawler(
                                     StringUtils.uncapitalize(methodMetaInfo.getMethodName()) + "MethodCrawlerImpl")
                             .get().transactionHandler(transaction, block.getTimestamp(), contractEntry,
-                                    methodMetaInfo.getMethodName(), txHashContractAddressMapping));
+                                    methodMetaInfo.getMethodName(), txHashContractAddressMapping).setMethodStatus(receipt.getStatus()));
                 }
             }
         }
